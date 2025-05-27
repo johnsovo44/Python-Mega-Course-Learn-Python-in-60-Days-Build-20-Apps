c7f8863899b6cfc5ffc79ce8800278b8ecb64b63
johnsovo44 <vonn.n.johnson@gmail.com>
2025-05-26 23:38:48 -0400

    todo app: Move helloworld.py to todo_app and add main.py

---
+++ todo_app/main.py
@@ -0,0 +1,25 @@
+"""
+Notes: 
+- Command line is used to interact with a script via text input and output.
+- You want to follow the input-processing-output pattern. The input is the input from the user, the processing is the logic of the script, and the output is what is displayed to the user. 
+- In the case of the to do list app, the input is the user's command (like adding a task), the processing is the logic to add that task to the list, and the output is the updated list of tasks.
+- main.py is the entry point of the application, where the main logic is executed.
+- remember to be literal when naming your variables, functions, and files. Makes readability of your code easier for future you and others.
+- Using intermediate variables, aka variables you don't need but are used to make the code more readable, is a good practice.
+
+Concepts Used in this Script:
+- Strings
+- Variables
+- main.py
+- input function
+"""
+# This is the main script for a simple todo application.
+
+# Below we use a variable to store the user input for a todo item.
+# A variable is a named storage location in memory that can hold data.
+user_prompt = "Enter todo:"  # This is a prompt for the user to enter a todo item.
+user_text = input(user_prompt)  # Get user input for the todo item
+# you can place text inside the input function to prompt the user.
+print("adding todo...")  # Notify the user that the todo is being added
+# Here we simulate adding the todo item to a list.
+print(f"Todo added: {user_text}")  # Output the added todo item
