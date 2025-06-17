27b669aa7e0abe69231259dff05683c0bd44acac
johnsovo44 <vonn.n.johnson@gmail.com>
2025-06-17 17:00:49 -0400

    implemented "adding new items" feature to web app

--- todo_app/App/web.py
+++ todo_app/App/web.py
@@ -2,6 +2,15 @@ import streamlit as st
 import modules.functions as f
 
 todos = f.get_todos()
+
+def add_todo():
+    new_todo = st.session_state["new_todo"] + "\n"
+    # session state is used to store the value of the input field
+    todos = f.get_todos()
+    todos.append(new_todo)
+    f.write_todos(todos)
+    st.session_state["new_todo"] = ""  # Clear the input field after adding
+
 st.title("To-Do App")
 st.subheader("Manage your tasks efficiently")
 st.write("This is a simple To-Do application built with Streamlit.")
@@ -10,4 +19,8 @@ st.write("You can add, edit, and delete tasks.")
 for todo in todos:
     st.checkbox(todo.strip(), key=todo)
 
-st.text_input(label=" ", placeholder="Add a new task", key="new_todo")
+st.text_input(label=" ", placeholder="Add a new task", key="new_todo"
+              , on_change = add_todo)
+
+st.session_state
+# this helps you see what is going on in the session state
